9883ace1131d5461aa9c23fdd572d5a1182ae944
gregalantiu2 <tiugreg2@gmail.com>
2021-02-09 23:03:11 -0500

    Added library and can succesully connect to data stream

---
+++ Keys/ApiKeys.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockTrading.Desktop.ApiKeys
+{
+    public static partial class APIKeys
+    {
+        public static readonly string ApiKey = "";
+        public static readonly string SecretKey = "";
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,11 @@
-﻿ using System;
+﻿using StockTrading.Desktop.ApiKeys;
+using StockTrading.Libraries;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace StockTrading.Desktop
 {
@@ -20,6 +14,7 @@ namespace StockTrading.Desktop
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Account account;
         public MainWindow()
         {
             InitializeComponent();
@@ -27,8 +22,13 @@ namespace StockTrading.Desktop
         private void MainWindow_Loaded(object sender,RoutedEventArgs e)
         {
             contentFrame.NavigationService.Navigate(new HomeWindow());
+            account = new Account(APIKeys.ApiKey,APIKeys.SecretKey);
+
+            var test = account.Run();
+            var test2 = account.buyingPower;
+
         }
-        
+
         private void AlgoLink_Clicked(object sender, RoutedEventArgs e)
         {
             contentFrame.NavigationService.Navigate(new AlgoWindow());
